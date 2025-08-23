87bfe2b01eeefef00620364c1a01e3803f0e3a10
Varshini <botlavarshini48@gmail.com>
2025-08-23 13:26:11 +0000

    add moving lines in background with small size . i think it looks more p

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -102,12 +102,17 @@ export default {
         'float': {
           '0%, 100%': { transform: 'translateY(0)' },
           '50%': { transform: 'translateY(-10px)' },
+        },
+        'moving-lines': {
+            '0%': { 'background-position': '0 0' },
+            '100%': { 'background-position': '56px 56px' },
         }
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         'float': 'float 4s ease-in-out infinite',
+        'moving-lines': 'moving-lines 4s linear infinite',
       },
     },
   },
