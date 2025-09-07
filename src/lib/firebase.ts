7551e2f98016e127b4bcb97c6c377ae379f990bf
Varshini <botlavarshini48@gmail.com>
2025-09-07 05:12:30 +0000

    Set up Firebase Backend

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  "projectId": "learnflowai-hk6c5",
-  "appId": "1:755832706965:web:5708a00cf1634aa199489c",
-  "storageBucket": "learnflowai-hk6c5.firebasestorage.app",
-  "apiKey": "AIzaSyC4sv2-RjJBQZ0EZe_LrbDFnVxKr34T3lg",
-  "authDomain": "learnflowai-hk6c5.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "755832706965"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
